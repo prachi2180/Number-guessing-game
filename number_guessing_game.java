31c9b4fa542293b8a8c703559abaa099cdea1f85
Prachiti kolhe <128719284+prachi2180@users.noreply.github.com>
2023-03-24 23:02:31 +0530

    Add files via upload

---
+++ number_guessing_game.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+import javax.swing.JOptionPane;
+public class number_guessing_game
+{
+    public static void main(String args[])
+    {
+        int computerNumber = (int) (Math.random()*100 + 1);
+        int userNumber = 0;
+        int count = 1;
+        try{
+            while(userNumber != computerNumber){
+                String response = JOptionPane.showInputDialog(null,"Enter a aguess betwn 1 and 100");
+                userNumber = Integer.parseInt(response);
+                JOptionPane.showMessageDialog(null, " " + determineGuess(userNumber , computerNumber,count));
+                count ++;
+            }
+        } catch (Exception e ){
+            //tooo : handle exception 
+        }
+    }
+    public static String determineGuess (int userNumber , int computerNumber , int count){
+        if (userNumber <=0 || userNumber >100){
+            return "YOUR GUESS IS INVALID ";
+        }else if(userNumber == computerNumber){
+            return "CORRECT \n TOTAL GUESSES : " + count ; 
+        }else if(userNumber > computerNumber){
+            return "YOUR GUESS IS TOO HIGH , TRY AGAIN \n TRY NUMBER :" + count;
+        }else if(userNumber < computerNumber){
+            return "YOUR GUESS IS TOO LOW  , TRY AGAIN \n TRY NUMBER :" + count;
+        }else {
+            return " YOUR GUESS IS INCORRECT \n TRY NUMBER :" +count;
+        }
+    }
+}
